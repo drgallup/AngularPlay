98ce123e841c03a4455a73d32d5b63e2928d55f1
imancini <imancini@ucsc.edu>
2017-04-16 19:02:58 -0700

    Add files via upload

--- Controller.js
+++ Controller.js
@@ -4,5 +4,5 @@ var appname = angular.module('appname', []);
 appname.controller('appCtrl', ['$scope',
   function($scope) {
     $scope.greeting = { text: 'Hello' };
-    $scope.date = new Date('2017', '04', '20');
+    $scope.date = Date('2017', '04', '20');
 }]);
