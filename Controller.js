5121ee69f48f6e3d498d04e08f976a49eb9f499e
drgmonkey <dylangallup@gmail.com>
2017-04-24 13:13:55 -0700

    Added a switch!

--- Controller.js
+++ Controller.js
@@ -1,8 +1,25 @@
 // controller.js
 
-var appname = angular.module('appname', []);
+angular.module('MyApp', ['ngMaterial'])
+.controller('appCtrl', function($scope) {
+  $scope.data = {
+    cb1: true,
+    cb4: true,
+    cb5: false
+  };
+
+  $scope.message = 'false';
+  
+  $scope.greeting = { text: 'Hello' };
+  $scope.date = Date('2017', '04', '20');
+
+  $scope.onChange = function(cbState) {
+  	$scope.message = cbState;
+  };
+});
+/*
 appname.controller('appCtrl', ['$scope',
   function($scope) {
     $scope.greeting = { text: 'Hello' };
     $scope.date = Date('2017', '04', '20');
-}]);
+}]);*/
